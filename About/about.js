4d3a8d9701f105bdf403c14d7be0a2679586ac3a
Blake <blake.p.preston@gmail.com>
2021-03-05 12:59:03 -0600

    Adding different sections to page

---
+++ About/about.js
@@ -0,0 +1,37 @@
+// Mobile Nav
+
+let menu_icon = document.getElementById("navBarMobile");
+let side_nav = document.getElementById("side_nav");
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+
+side_nav.style.right = "-360px";
+
+menu_icon.onclick = function () {
+  if (side_nav.style.right == "-360px") {
+    side_nav.style.right = "0";
+  } else {
+    side_nav.style.right = "-360px";
+  }
+};
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navTop").style.top = "0";
+  } else {
+    document.getElementById("navTop").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
